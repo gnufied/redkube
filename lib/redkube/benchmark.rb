926b07e51bf06a4166e04e845f51524c75dc294d
Hemant Kumar <hekumar@redhat.com>
2017-09-25 14:25:15 -0400

    commiting uncommited changes on 2017-09-25 14:25:15 -0400

--- lib/redkube/benchmark.rb
+++ lib/redkube/benchmark.rb
@@ -1,7 +1,7 @@
 module RedKube
   class Benchmark
     def initialize
-      @benchmark_results = Hash.new() { |h, k|  h[k] = [] }
+      @benchmark_results = {}
     end
 
     def benchmark(key)
@@ -9,6 +9,7 @@ module RedKube
         t1 = Time.now()
         yield i
         diff = Time.now() - t1
+        @benchmark_results[key] ||= []
         @benchmark_results[key][i] << diff
       end
       print_results()
