42eda40cdf70d0460cbe775f2c0394f72b94f507
Hemant Kumar <hekumar@redhat.com>
2017-10-04 17:58:52 -0400

    commiting uncommited changes on 2017-10-04 17:58:52 -0400

--- lib/redkube/pod_mark.rb
+++ lib/redkube/pod_mark.rb
@@ -20,6 +20,7 @@ module RedKube
 
       benchmark("* create_delete_loop", 100) do |i|
         benchmark(">> pod_delete_recreate", 1) do |index|
+          t1 = Time.now
           pod_name = "dyn-pod-#{index}"
           pvc_name = "dyn-pvc-#{index}"
           puts "Recreating pod #{pod_name}"
@@ -28,8 +29,12 @@ module RedKube
 
           pod.delete
 
+          puts "Time taken to delete pod #{Time.now() - t1}"
+
+          t2 = Time.now()
           pod = Pod.from_erb(pod_yaml, pod_name, pvc_name)
           pod.check_for_pod
+          puts "Time taken to create pod #{Time.now() - t2}"
         end
       end
     end
